b0a274943ef30badf7b5cf712c42f6e631d291c5
Arnob-Ghosh <94540571+iarko26@users.noreply.github.com>
2024-05-16 19:00:15 +0600

    Second Day Commit
    1. Create Server
    2. GET METHOD

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-//importing modules
-const notes=require('./notes.js');
-var age=notes.age;
+//create a server
+const express = require('express')
+const app = express(); //create an instance of express
 
-var sum=notes.addnum(age,10);
-console.log(sum);
-//lodash
-var _ = require('lodash');
-var array=["person","person",1,2,3,"name","profession","age"];
-var filter=_.uniq(array);
-console.log(filter);
-var type=_.isString('person');
-console.log(type);
+app.get('/', function (req, res) {
+  res.send('Hello thsi is node js');
+})
+app.get('/about',function(req,res){
+    res.send('This is about page');
+})
+app.get('/tech',function(req,res){
+    var techstack={
+        frontend:'React',
+        backend:'NodeJS',
+        database:'MongoDB',
+        isdatabase:'Yes',
+        isfrontend:'Yes',
 
+    }
+    res.send(techstack);
+})
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
+
+//to shut down the server press ctrl+c
+//to restart the server press up arrow and enter
+//port number 3000
 

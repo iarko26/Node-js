ea0d81c5f1ad0f78e1e86edbc6609cdadfba9406
Arnob-Ghosh <94540571+iarko26@users.noreply.github.com>
2024-05-17 22:09:32 +0600

    Mongodb commit
    Connection with mongodb using mongoose (mongodb compass)
    text file (mongoshell)

---
+++ server3.js
@@ -0,0 +1,88 @@
+//create a server
+const express = require('express'); //import express
+const app = express(); //create an instance of express
+//routes
+app.get('/', function (req, res) {
+  res.send('Hello thsi is node js');
+})
+app.get('/about',function(req,res){
+    res.send('This is about page');
+})
+app.get('/tech',function(req,res){
+    var techstack={
+        frontend:'React',
+        backend:'NodeJS',
+        database:'MongoDB',
+        isdatabase:'Yes',
+        isfrontend:'Yes',
+
+    }
+    res.send(techstack);
+});
+//use parse request body data in express  especially for post or put request
+const bodyParser=require('body-parser');
+//parse json data and add to req.body
+app.use(bodyParser.json());
+//post request
+app.post('/api/cars',function(req,res){
+    const{name,brand}=req.body;
+    console.log(name);
+    console.log(brand);
+    res.send('Car is created');
+
+});
+
+//link to mongodb
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost:27017/cars',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+
+    
+})
+.then(()=>{
+    console.log('Connected to mongodb');
+
+})
+.catch(()=>{
+    console.log('Failed to Connect to mongodb');
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// get request with query parameters
+app.get('/api/cars',(req,res)=>{
+    const cars=[
+        {id:1,name:'Audi',brand:'Audi'},
+        {id:2,name:'BMW',brand:'BMW'},
+        {id:3,name:'Mercedes',brand:'Mercedes'},
+        {id:4,name:'Jaguar',brand:'Jaguar'},
+    ];
+    res.send(cars);
+});
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
+
+//to shut down the server press ctrl+c
+//to restart the server press up arrow and enter
+//port number 3000
+
